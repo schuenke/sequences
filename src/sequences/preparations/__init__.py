e4ba3d3b6448e59cebbcc9a28c4ff806b11ef22a
Patrick Schuenke <patrick.schuenke@gmail.com>
2025-01-16 17:26:34 +0100

    add t1_inv_rec_gre_single_line script

--- src/sequences/preparations/__init__.py
+++ src/sequences/preparations/__init__.py
@@ -1,4 +1,6 @@
+from sequences.preparations.t1prep import add_t1prep
 from sequences.preparations.t2prep import add_composite_refocusing_block
 from sequences.preparations.t2prep import add_t2prep
 
-__all__ = ['add_composite_refocusing_block', 'add_t2prep']
+
+__all__ = ['add_composite_refocusing_block', 'add_t1prep', 'add_t2prep']
